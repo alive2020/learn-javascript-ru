79ae821c7f086f7e475721648f424f7ff815206f
Ice K <alive.k001@gmail.com>
2021-12-07 16:07:42 +0900

    added tasks from alertpromt, ifelse part

---
+++ Part1/javascriptBasics/alertprompt.js
@@ -0,0 +1,7 @@
+let name = prompt('What is your name?', '');
+alert(name ? `Your name is ${name}` : 'You did not write your name');
+
+let a = prompt('Первое число?', +1);
+let b = prompt('Второе число?', +2);
+
+alert(+a + +b); // 12
---
+++ Part1/javascriptBasics/ifelse.js
@@ -0,0 +1,6 @@
+let jsOfficialName = prompt('Какое «официальное» название JavaScript?', '');
+if (jsOfficialName === 'ECMAScript') {
+  alert('Верно!');
+} else {
+  alert('Не знаете? ECMAScript!');
+}
