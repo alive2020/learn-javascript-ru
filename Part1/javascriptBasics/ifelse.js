9b59b3ae83ee6303c4dc5ab5592a6f69b856f826
Ice K <alive.k001@gmail.com>
2021-12-10 13:01:04 +0900

    task:logical operators 2.11 tasks

--- Part1/javascriptBasics/ifelse.js
+++ Part1/javascriptBasics/ifelse.js
@@ -1,39 +1,38 @@
-// let jsOfficialName = prompt('Какое «официальное» название JavaScript?', '');
-// if (jsOfficialName === 'ECMAScript') {
-//   alert('Верно!');
-// } else {
-//   alert('Не знаете? ECMAScript!');
-// }
+let jsOfficialName = prompt('Какое «официальное» название JavaScript?', '');
+if (jsOfficialName === 'ECMAScript') {
+  alert('Верно!');
+} else {
+  alert('Не знаете? ECMAScript!');
+}
 
-// let num = prompt('Напишите число!', '');
-// if (num > 0) {
-//   alert(1);
-// } else if (num < 0) {
-//   alert(-1);
-// } else {
-//   alert(0);
-// }
+let num = prompt('Напишите число!', '');
+if (num > 0) {
+  alert(1);
+} else if (num < 0) {
+  alert(-1);
+} else {
+  alert(0);
+}
 
-// let a = prompt('Enter a number?', '');
-// let b = prompt('Enter a number?', '');
-// alert(+a + +b < 4 ? 'Small' : 'Big');
+let a = prompt('Enter a number?', '');
+let b = prompt('Enter a number?', '');
+alert(+a + +b < 4 ? 'Small' : 'Big');
 
-// let result = (a + b < 4) ? 'Small' : 'Big';
+let result = (a + b < 4) ? 'Small' : 'Big';
 
+let login = prompt('Input your login!', '');
 
-
-// let login = prompt('Input your login!', '');
-
-// alert(
-//   login == 'Сотрудник'
-//     ? 'Привет'
-//     : login == 'Директор'
-//     ? 'Здравствуйте'
-//     : login == ''
-//     ? 'Нет логина'
-//     : ''
-// );
+alert(
+  login == 'Сотрудник'
+    ? 'Привет'
+    : login == 'Директор'
+    ? 'Здравствуйте'
+    : login == ''
+    ? 'Нет логина'
+    : ''
+);
 
 let message = (login == 'Сотрудник') ? 'Привет' :
 (login == 'Директор') ? 'Здравствуйте' :
 (login == '') ? 'Нет логина' : '' ;
+
---
+++ Part1/javascriptBasics/logicalOperands.js
@@ -0,0 +1,42 @@
+let age = 4;
+if (age >= 14 && age <= 90) {
+//   alert(`${age} is between 14 and 90`);
+}
+
+if(!(age>=14 && age <=90)){
+    // alert(`${age} is not between (14 and 90)` )
+ 
+}
+
+if(age < 14 || age > 90){
+//   alert(`${age} is not between 14 and 90` )
+}
+
+
+// Напишите код, который будет спрашивать логин с помощью prompt.
+// Если посетитель вводит «Админ», то prompt запрашивает пароль, если ничего не введено или нажата клавиша Esc – показать «Отменено», в противном случае отобразить «Я вас не знаю».
+// Пароль проверять так:
+// Если введён пароль «Я главный», то выводить «Здравствуйте!»,
+// Иначе – «Неверный пароль»,
+// При отмене – «Отменено».
+
+let userName = prompt('Who are you?', "")
+let password = "";
+
+if( userName === "Admin"){
+    
+    password = prompt("Enter your password!", "")
+    if(password === "I am the Boss") {
+        alert("Hello Boss!")
+    } else if(password === "" || password === null ) {
+        alert("Canceled")
+    } else {
+        alert("Invalid Password!")
+    }
+
+} else if(userName === "" || userName === null ) {
+    alert("Canceled")
+} else {
+    alert('I dont know you!')
+}
+
