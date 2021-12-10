72159cf4f2eb38ae78087cc3185ab0c309bf333c
Ice K <alive.k001@gmail.com>
2021-12-10 16:45:34 +0900

    task: switch, func tasks

---
+++ Part1/javascriptBasics/func.js
@@ -0,0 +1,33 @@
+let age = prompt('age', '');
+
+function checkAge(age) {
+  return age > 18 ? true : confirm('Родители разрешили?');
+}
+
+function checkAge(age) {
+  return age > 18 || confirm('Родители разрешили?');
+}
+
+function min(a, b) {
+     if(a > b) return b
+     return a
+}
+
+function min(a, b) {
+  return a < b ? a : b;
+}
+alert(min(2, 5));
+alert(min(2, -1));
+
+
+
+function power(a, b) {
+    // return a ** b
+ let res = a;  
+ for(let i = 1; i < b; i++){
+     res *= a 
+ }            
+ return res;
+}
+alert(power(3, 4));
+alert(power(3, 2));
