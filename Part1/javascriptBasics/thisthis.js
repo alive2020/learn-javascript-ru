af0158ecce6992baeedcd85594201a56ef511dbc
Ice K <alive.k001@gmail.com>
2021-12-27 11:07:50 +0900

    context to func tasks

---
+++ Part1/javascriptBasics/thisthis.js
@@ -0,0 +1,45 @@
+function askPassword(ok, fail) {
+  let password = prompt('Password?', '');
+  if (password == 'rockstar') ok();
+  else fail();
+}
+
+let user = {
+  name: 'Вася',
+
+  loginOk() {
+    alert(`${this.name} logged in`);
+  },
+
+  loginFail() {
+    alert(`${this.name} failed to log in`);
+  },
+};
+
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+
+askPassword(
+  () => user.loginOk(),
+  () => user.loginFail()
+);
+
+//////////////////////////////////////////////
+function askPassword(ok, fail) {
+  let password = prompt('Password?', '');
+  if (password == 'rockstar') ok();
+  else fail();
+}
+
+let user = {
+  name: 'John',
+
+  login(result) {
+    console.log(this.name + (result ? ' logged in' : ' failed to log in'));
+  },
+};
+
+askPassword(user.login.bind(user, true), user.login.bind(user, false)); // ?
+askPassword(
+  () => user.login(true),
+  () => user.login(false)
+);
